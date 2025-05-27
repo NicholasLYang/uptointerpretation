2ebd94c346ee8ef86d31343d7dc8d98357c2f6ae
Nicholas Yang <nick@nicholasyang.com>
2025-05-27 19:39:56 -0400

    Some more tweaking

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://uptointerpretation.com',
   integrations: [mdx(), sitemap()],
 
   vite: {
